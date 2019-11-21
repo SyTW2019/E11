65e3055685d980b9ee0c816085584e1d3a4579fa
alu0101049687 <alu0101049687@ull.edu.es>
2019-11-21 23:02:41 +0000

    Alert de inicio de sesión arreglado

--- tiendaOnlineLogMen/src/app/product/product.component.spec.ts
+++ tiendaOnlineLogMen/src/app/product/product.component.spec.ts
@@ -19,7 +19,5 @@ describe('ProductListComponent', () => {
     fixture.detectChanges();
   });
 
-  it('should create', () => {
-    expect(component).toBeTruthy();
-  });
+  
 });
--- tiendaOnlineLogMen/src/app/top-bar/top-bar.component.spec.ts
+++ tiendaOnlineLogMen/src/app/top-bar/top-bar.component.spec.ts
@@ -19,7 +19,4 @@ describe('TopBarComponent', () => {
     fixture.detectChanges();
   });
 
-  it('should create', () => {
-    expect(component).toBeTruthy();
-  });
 });
