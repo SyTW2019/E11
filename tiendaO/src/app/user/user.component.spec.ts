7603dd10bd5cc652b5fc6224faeb2ebd8f5bb34e
Zuzanna Elzbieta Szalaty <47361285+alu0101049687@users.noreply.github.com>
2020-01-26 18:17:12 +0000

    Test usuario.

--- tiendaO/src/app/user/user.component.spec.ts
+++ tiendaO/src/app/user/user.component.spec.ts
@@ -22,4 +22,35 @@ describe('UserListComponent', () => {
   it('should create', () => {
     expect(component).toBeTruthy();
   });
+
+  it(`should have an id`, () => {
+    const fixture = TestBed.createComponent(UserListComponent);
+    const user = fixture.debugElement.componentInstance;
+    expect(user.id).toEqual(user.id);
+  });
+
+  it(`should have an Firs Name`, () => {
+    const fixture = TestBed.createComponent(UserListComponent);
+    const user = fixture.debugElement.componentInstance;
+    expect(user.firstName).not.toBeNull();
+  });
+
+  it(`should have an Last Name`, () => {
+    const fixture = TestBed.createComponent(UserListComponent);
+    const user = fixture.debugElement.componentInstance;
+    expect(user.lastName).not.toBeNull();
+  });
+
+  it(`should have an password`, () => {
+    const fixture = TestBed.createComponent(UserListComponent);
+    const user = fixture.debugElement.componentInstance;
+    expect(user.password).not.toBeNull();
+  });
+
+  it(`should have an email`, () => {
+    const fixture = TestBed.createComponent(UserListComponent);
+    const user = fixture.debugElement.componentInstance;
+    expect(user.email).toEqual(user.email);
+  });
 });
+
