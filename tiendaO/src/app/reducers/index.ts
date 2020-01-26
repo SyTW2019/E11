bfa7d412da585b1c292f3764e2915036b47f4023
alu0101372617 <alu0101372617@ull.edu.es>
2020-01-26 15:46:21 +0000

    Arreglar bugs de Sonarcloud

--- tiendaO/src/app/reducers/index.ts
+++ tiendaO/src/app/reducers/index.ts
@@ -16,4 +16,4 @@ export const reducers: ActionReducerMap<State> = {
 };
 
 
-export const metaReducers: MetaReducer<State>[] = !environment.production ? [] : [];
+export const metaReducers: MetaReducer<State>[] =  [];
